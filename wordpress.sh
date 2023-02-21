7d0bce8a1249f18ad5221dfec86429f1bb8a8464
naidu456 <118648202+naidu456@users.noreply.github.com>
2023-02-21 16:00:57 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update
 sudo yum -y install git
-sudo git clone https://github.com/naidu456/docker-compose.yaml.git
+sudo git clone https://github.com/naidu456/docker-compose.yml.git
 sudo yum -y install docker
 sudo usermod -a -G docker ec2-user
 sudo systemctl start docker
@@ -9,5 +9,5 @@ sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-cd docker-compose.yaml
+cd docker-compose.yml
 sudo docker-compose up -d
